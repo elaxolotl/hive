171165fb5c01407f68de75bbc9e5e189938c22e6
youssefEchadySfaxi <youssefechadysfaxi@gmail.com>
2025-03-23 12:27:52 +0100

    style: re-organized GUI elements

--- BoardGUI.java
+++ BoardGUI.java
@@ -2,127 +2,135 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import cells.Cell;
 
 public class BoardGUI extends JPanel {
     private final Board board;
-    private JSlider speedSlider;
+    private final JSlider speedSlider;
+    private final JSlider lifespanSlider;
+    private final JButton startButton;
+    private final JButton resetButton;
+    private final JLabel speedLabel;
+    private final JLabel lifespanLabel;
     private boolean running = false;
     private int speed;
+    private final BoardPanel boardPanel;
 
     public BoardGUI(Board board) {
         this.board = board;
+        this.speedSlider = new JSlider(100, 5000, 100);
+        this.lifespanSlider = new JSlider(3, 200, 100);
+        this.startButton = new JButton("Start");
+        this.resetButton = new JButton("Reset");
+        this.speedLabel = new JLabel("Speed: " + speedSlider.getValue());
+        this.lifespanLabel = new JLabel("LifeSpan: " + lifespanSlider.getValue());
+        this.boardPanel = new BoardPanel();
         setupUI();
+        addListeners();
     }
 
     private void setupUI() {
-        speedSlider = new JSlider(100, 5000, 100);
-        JSlider lifespanSlider = new JSlider(10, 200, 100);
-        JButton startButton = new JButton("Start");
-        JButton stopButton = new JButton("Stop");
-        JButton resetButton = new JButton("Reset");
-
-        startButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                running = true;
-                new Thread(() -> runSimulation()).start();
-            }
-        });
-
-        stopButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                running = false;
-            }
-        });
-
-        resetButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                running = false;
-                board.reset();
-                repaint();
-            }
-        });
-
-        lifespanSlider.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                Cell.setMaxLifespan(lifespanSlider.getValue());
-
-            }
-        });
-
-        speedSlider.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                speed = speedSlider.getValue();
-            }
-        });
-
         this.setLayout(new BorderLayout());
-        JPanel controlPanel = new JPanel();
-        controlPanel.setLayout(new GridLayout(2, 3));
-        controlPanel.add(new JLabel("Speed:"));
-        controlPanel.add(new JLabel(String.valueOf(speed)));
-        controlPanel.add(speedSlider);
-        controlPanel.add(startButton);
-        controlPanel.add(new JLabel("Lifespan:"));
-        controlPanel.add(new JLabel(String.valueOf(lifespanSlider.getValue())));
-        controlPanel.add(lifespanSlider);
-        controlPanel.add(stopButton);
-        controlPanel.add(resetButton);
 
-        this.add(controlPanel, BorderLayout.SOUTH);
+        JPanel controlPanel = new JPanel(new GridLayout(2, 2, 5, 5));
+        controlPanel.add(speedLabel);
+        controlPanel.add(speedSlider);
+        controlPanel.add(lifespanLabel);
+        controlPanel.add(lifespanSlider);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        buttonPanel.add(startButton);
+        buttonPanel.add(resetButton);
+
+        this.add(controlPanel, BorderLayout.NORTH);
+        this.add(boardPanel, BorderLayout.CENTER);
+        this.add(buttonPanel, BorderLayout.SOUTH);
     }
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Cell[][] grid = board.getGrid();
-        int cellSize = 20;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j].getAlive()) {
-                    if (grid[i][j].getAge() == 1) {
-                        g.setColor(Color.BLACK);
-                    } else if (grid[i][j].getAge() == 2) {
-                        g.setColor(Color.DARK_GRAY);
-                    } else if (grid[i][j].getAge() == 3) {
-                        g.setColor(Color.GRAY);
-                    }
-                } else {
-                    g.setColor(Color.WHITE);
-                }
-                g.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
-                g.setColor(Color.WHITE);
-                g.drawRect(j * cellSize, i * cellSize, cellSize, cellSize);
-            }
+    private void addListeners() {
+        startButton.addActionListener(e -> toggleSimulation());
+        resetButton.addActionListener(e -> resetBoard());
+        lifespanSlider.addChangeListener(e -> updateLifespan());
+        speedSlider.addChangeListener(e -> updateSpeed());
+    }
+
+    private void toggleSimulation() {
+        running = !running;
+        startButton.setText(running ? "Stop" : "Start");
+        if (running) {
+            new Thread(this::runSimulation).start();
         }
+    }
+
+    private void resetBoard() {
+        running = false;
+        board.reset();
+        boardPanel.repaint();
+    }
+
+    private void updateLifespan() {
+        int newLifespan = lifespanSlider.getValue();
+        Cell.setMaxLifespan(newLifespan);
+        lifespanLabel.setText("LifeSpan: " + newLifespan);
+    }
+
+    private void updateSpeed() {
+        speed = speedSlider.getValue();
+        speedLabel.setText("Speed: " + speed);
     }
 
     private void runSimulation() {
         while (running) {
             board.updateGrid();
-            repaint();
+            boardPanel.repaint();
             try {
                 Thread.sleep(speedSlider.getValue());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
             }
+        }
+    }
+
+    private class BoardPanel extends JPanel {
+        private final int cellSize = 20;
+
+        public BoardPanel() {
+            int width = board.getGrid()[0].length * cellSize;
+            int height = board.getGrid().length * cellSize;
+            setPreferredSize(new Dimension(width, height));
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Cell[][] grid = board.getGrid();
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[i].length; j++) {
+                    g.setColor(grid[i][j].getAlive() ? getCellColor(grid[i][j].getAge()) : Color.WHITE);
+                    g.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
+                }
+            }
+        }
+
+        private Color getCellColor(int age) {
+            return switch (age) {
+                case 1 -> Color.BLACK;
+                case 2 -> Color.DARK_GRAY;
+                case 3 -> Color.GRAY;
+                default -> Color.WHITE;
+            };
         }
     }
 
     public static void main(String[] args) {
         Board board = new Board(35, 35);
-        BoardGUI boardGUI = new BoardGUI(board);
-        JFrame frame = new JFrame("Board");
+        JFrame frame = new JFrame("Game of Hive");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 600);
-        frame.add(boardGUI);
+        BoardGUI boardGUI = new BoardGUI(board);
+        frame.getContentPane().add(boardGUI);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 }
